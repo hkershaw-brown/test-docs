81035240ff6197375577b298b11b4849d07b7fd8
Helen Kershaw <hkershaw@ucar.edu>
2021-03-31 13:08:57 -0600

    adding a tag to manhattan conf.py

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ copyright = '2021, University Corporation for Atmospheric Research'
 author = 'Data Assimilation Research Section'
 
 # The full version, including alpha/beta/rc tags
-release = 'Hello'
+release = '12.13'
 master_doc = 'README'
 
 # -- General configuration ---------------------------------------------------
